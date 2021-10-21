217585c8cb8f10c48eec647b3b14bc7569b2f672
LuckDuracell <lukedru@yahoo.com>
2021-10-21 11:26:01 -0700

    Initial Commit

---
+++ LuckDev Apps/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  LuckDev Apps
+//
+//  Created by Luke Drushell on 10/21/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ LuckDev Apps/LuckDev_AppsApp.swift
@@ -0,0 +1,17 @@
+//
+//  LuckDev_AppsApp.swift
+//  LuckDev Apps
+//
+//  Created by Luke Drushell on 10/21/21.
+//
+
+import SwiftUI
+
+@main
+struct LuckDev_AppsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
